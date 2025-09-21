f586476753b495df09547be5d349c5ab98a56f7e
Alejo <alejoivanrojas@gmail.com>
2025-09-21 10:11:55 -0300

    icpc setup

---
+++ libs.h
@@ -0,0 +1,33 @@
+#ifndef MYSTD_H
+#define MYSTD_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <fstream>
+#include <bitset>
+#include <tuple>
+#include <limits>
+#include <utility>
+#include <iterator>
+#include <cassert>
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "libs.h"
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+template<typename T>
+void printVector(const vector<T> &v) {
+    for (const auto &el : v) {
+        cout << el << " ";
+    }
+    cout << endl;
+}
+
+
+int main() {
+    cout << "hi from here";
+
+    return 0;
+}
